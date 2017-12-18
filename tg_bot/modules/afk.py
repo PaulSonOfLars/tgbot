2e91074298ef885f2efe93244ebd1a5676aaf2ec
Paul Larsen <pn1315@ic.ac.uk>
2017-12-18 16:42:01 +0000

    Add new afk module

---
+++ tg_bot/modules/afk.py
@@ -0,0 +1,80 @@
+from telegram import MessageEntity
+
+from tg_bot.modules.sql import afk_sql as sql
+from telegram.ext import CommandHandler, Filters, MessageHandler, run_async
+from tg_bot import dispatcher
+from tg_bot.modules.users import get_user_id
+
+AFK_GROUP = 7
+AFK_REPLY_GROUP = 8
+
+
+@run_async
+def afk(bot, update):
+    args = update.effective_message.text.split(None, 1)
+    if len(args) >= 2:
+        reason = args[1]
+    else:
+        reason = ""
+
+    sql.set_afk(update.effective_user.id, reason)
+    update.effective_message.reply_text("No problem {}, you've been set as AFK!".format(update.effective_user.first_name))
+
+
+@run_async
+def no_longer_afk(bot, update):
+    user = update.effective_user
+    res = sql.rm_afk(user.id)
+    if res:
+        update.effective_message.reply_text("{} is no longer AFK!".format(update.effective_user.first_name))
+
+
+@run_async
+def reply_afk(bot, update):
+    message = update.effective_message
+    if message.entities and message.parse_entities([MessageEntity.TEXT_MENTION]):
+        entities = message.parse_entities([MessageEntity.TEXT_MENTION])
+        for e in entities:
+            user_id = e.user.id
+            user = sql.check_afk_status(user_id)
+            if user and user.is_afk:
+                if not user.reason:
+                    res = "{} is AFK!".format(e.user.first_name)
+                else:
+                    res = "{} is AFK! says its because of:\n{}".format(e.user.first_name, user.reason)
+                message.reply_text(res)
+        else:
+            return
+    elif message.entities and message.parse_entities([MessageEntity.MENTION]):
+        entities = message.parse_entities([MessageEntity.MENTION])
+        for e in entities:
+            user_id = get_user_id(message.text[e.offset:e.offset+e.length])
+            if not user_id:
+                # Should never happen, since for a user to become AFK they must have spoken. Maybe changed username?
+                return
+            user = sql.check_afk_status(user_id)
+            if user and user.is_afk:
+                chat = bot.get_chat(user_id)
+                if not user.reason:
+                    res = "{} is AFK!".format(chat.first_name)
+                else:
+                    res = "{} is AFK! says its because of:\n{}".format(chat.first_name, user.reason)
+                message.reply_text(res)
+        else:
+            return
+    else:
+        return
+
+
+__help__ = """
+ - /afk <reason>: mark yourself as AFK - any mentions will be replied to with a message to say you're away from \
+keyboard!
+"""
+
+AFK_HANDLER = CommandHandler("afk", afk)
+NO_AFK_HANDLER = MessageHandler(Filters.all, no_longer_afk)
+AFK_REPLY_HANDLER = MessageHandler(Filters.entity(MessageEntity.MENTION) | Filters.entity(MessageEntity.TEXT_MENTION), reply_afk)
+
+dispatcher.add_handler(AFK_HANDLER)
+dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
+dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
---
+++ tg_bot/modules/sql/afk_sql.py
@@ -0,0 +1,76 @@
+import threading
+
+from sqlalchemy import Column, UnicodeText, Boolean, Integer
+
+from tg_bot.modules.sql import BASE, SESSION
+
+
+class AFK(BASE):
+    __tablename__ = "afk_users"
+
+    user_id = Column(Integer, primary_key=True)
+    is_afk = Column(Boolean)
+    reason = Column(UnicodeText)
+
+    def __init__(self, user_id, reason="", is_afk=True):
+        self.user_id = user_id
+        self.reason = reason
+        self.is_afk = is_afk
+
+    def __repr__(self):
+        return "afk_status for {}".format(self.user_id)
+
+
+AFK.__table__.create(checkfirst=True)
+INSERTION_LOCK = threading.Lock()
+KEYSTORE = {}
+
+
+# check if need insertion lock
+def check_afk_status(user_id):
+    return SESSION.query(AFK).get(user_id)
+
+
+def set_afk(user_id, reason=""):
+    with INSERTION_LOCK:
+        curr = SESSION.query(AFK).get(user_id)
+        if not curr:
+            curr = AFK(user_id, reason, True)
+        else:
+            curr.is_afk = True
+        SESSION.add(curr)
+        SESSION.commit()
+
+
+def rm_afk(user_id):
+    with INSERTION_LOCK:
+        curr = SESSION.query(AFK).get(user_id)
+        if curr:
+            SESSION.delete(curr)
+            SESSION.commit()
+            return True
+        return False
+
+
+def toggle_afk(user_id, reason=""):
+    with INSERTION_LOCK:
+        curr = SESSION.query(AFK).get(user_id)
+        if not curr:
+            curr = AFK(user_id, reason, True)
+        elif curr.is_afk:
+            curr.is_afk = False
+        elif not curr.is_afk:
+            curr.is_afk = True
+        SESSION.add(curr)
+        SESSION.commit()
+
+
+def load_keystore():
+    with INSERTION_LOCK:
+        all_users = SESSION.query(AFK).all()
+        for user in all_users:
+            KEYSTORE[user.user_id].append(user)
+        SESSION.close()
+        print("{} total warning filters added to {} chats.".format(len(all_users), len(KEYSTORE)))
+
+load_keystore()
