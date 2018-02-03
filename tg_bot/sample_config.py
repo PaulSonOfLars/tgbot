7feaf9a91cf2f1dfafb058df09f18af4c9cbbf5e
Paul Larsen <pn1315@ic.ac.uk>
2018-02-03 14:01:29 +0000

    Quit bot if sample config is renamed.

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -1,3 +1,10 @@
+if not __name__.endswith("sample_config"):
+    import sys
+    print("The README is there to be read. Extend this sample config to a config file, don't just rename and change "
+          "values here. Doing that WILL backfire on you.\nBot quitting.", file=sys.stderr)
+    quit(1)
+
+
 # Create a new config.py file in same dir and import, then extend this class.
 class Config(object):
     LOGGER = True
