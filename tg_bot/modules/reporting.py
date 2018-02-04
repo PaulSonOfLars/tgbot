f342109d7263a5152ac2bbb04d943d78fddcf333
Paul Larsen <pn1315@ic.ac.uk>
2018-02-04 15:20:49 +0000

    Add /report and @admin to the list of available commands

---
+++ tg_bot/modules/reporting.py
@@ -0,0 +1,108 @@
+from typing import Optional, List
+
+from telegram import Message, Chat, Update, Bot, User
+from telegram.error import BadRequest, Unauthorized
+from telegram.ext import CommandHandler, RegexHandler, run_async
+
+from tg_bot import dispatcher, LOGGER
+from tg_bot.modules.helper_funcs.chat_status import user_not_admin, user_admin
+from tg_bot.modules.sql import reporting_sql as sql
+
+REPORT_GROUP = 5
+
+
+@run_async
+@user_admin
+def report_setting(bot: Bot, update: Update, args: List[str]):
+    chat = update.effective_chat  # type: Optional[Chat]
+    msg = update.effective_message  # type: Optional[Message]
+
+    if chat.type == chat.PRIVATE:
+        if len(args) >= 1:
+            if args[0] in ("yes", "on"):
+                sql.set_user_setting(chat.id, True)
+                msg.reply_text("Turned on reporting! You'll be notified whenever anyone reports something.")
+
+            elif args[0] in ("no", "off"):
+                sql.set_user_setting(chat.id, False)
+                msg.reply_text("Turned off reporting! You wont get any reports.")
+        else:
+            msg.reply_text("Your current report preference is: `{}`".format(sql.user_should_report(chat.id)))
+
+    else:
+        if len(args) >= 1:
+            if args[0] in ("yes", "on"):
+                sql.set_chat_setting(chat.id, True)
+                msg.reply_text("Turned on reporting! Admins who have turned on reports will be notified when /report "
+                               "or @admin are called.")
+
+            elif args[0] in ("no", "off"):
+                sql.set_chat_setting(chat.id, False)
+                msg.reply_text("Turned off reporting! No admins will be notified on /report or @admin.")
+        else:
+            msg.reply_text("This chat's current setting is: `{}`".format(sql.chat_should_report(chat.id)))
+
+
+@run_async
+@user_not_admin
+def report(bot: Bot, update: Update):
+    message = update.effective_message  # type: Optional[Message]
+    chat = update.effective_chat  # type: Optional[Chat]
+    user = update.effective_user  # type: Optional[User]
+
+    if chat and message.reply_to_message and sql.chat_should_report(chat.id):
+        chat_name = chat.title or chat.username or chat.first
+        admin_list = chat.get_administrators()
+        for admin in admin_list:
+            if admin.user.is_bot:  # can't message bots
+                continue
+
+            if sql.user_should_report(admin.user.id):
+                try:
+                    bot.send_message(admin.user.id,
+                                     "{} is calling for admins in {}!".format(user.first_name, chat_name))
+                    message.reply_to_message.forward(admin.user.id)
+
+                    if len(message.text.split()) > 1:  # If user is giving a reason, send his message too
+                        message.forward(admin.user.id)
+
+                except Unauthorized:
+                    pass
+                except BadRequest as excp:  # TODO: cleanup exceptions
+                    LOGGER.exception("Exception while reporting user")
+
+
+def __migrate__(old_chat_id, new_chat_id):
+    sql.migrate_chat(old_chat_id, new_chat_id)
+
+
+def __chat_settings__(chat_id, user_id):
+    return "This chat is setup to send user reports to admins, via /report and @admin: `{}`".format(
+        sql.chat_should_report(chat_id))
+
+
+def __user_settings__(user_id):
+    return "You receive reports from chats you're admin in: `{}`.\nToggle this with /reports in PM.".format(
+        sql.user_should_report(user_id))
+
+
+__name__ = "Reporting"
+
+__help__ = """
+ - /report <reason>: reply to a message to report it to admins.
+ - @admins: reply to a message to report it to admins.
+NOTE: neither of these will get triggered if used by admins
+
+*Admin only:*
+ - /reports <on/off>: change report setting, or view current status.
+   - If done in pm, toggles your status.
+   - If in chat, toggles that chat's status.
+"""
+
+REPORT_HANDLER = CommandHandler("report", report)
+SETTING_HANDLER = CommandHandler("reports", report_setting, pass_args=True)
+ADMIN_REPORT_HANDLER = RegexHandler("(?i)@admin", report)
+
+dispatcher.add_handler(REPORT_HANDLER, REPORT_GROUP)
+dispatcher.add_handler(ADMIN_REPORT_HANDLER, REPORT_GROUP)
+dispatcher.add_handler(SETTING_HANDLER)
---
+++ tg_bot/modules/sql/reporting_sql.py
@@ -0,0 +1,88 @@
+import threading
+from typing import Union
+
+from sqlalchemy import Column, Integer, String, Boolean
+
+from tg_bot.modules.sql import SESSION, BASE
+
+
+class ReportingUserSettings(BASE):
+    __tablename__ = "user_report_settings"
+    user_id = Column(Integer, primary_key=True)
+    should_report = Column(Boolean, default=True)
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+    def __repr__(self):
+        return "<User report settings ({})>".format(self.user_id)
+
+
+class ReportingChatSettings(BASE):
+    __tablename__ = "chat_report_settings"
+    chat_id = Column(String(14), primary_key=True)
+    should_report = Column(Boolean, default=True)
+
+    def __init__(self, chat_id):
+        self.chat_id = str(chat_id)
+
+    def __repr__(self):
+        return "<Chat report settings ({})>".format(self.chat_id)
+
+
+ReportingUserSettings.__table__.create(checkfirst=True)
+ReportingChatSettings.__table__.create(checkfirst=True)
+
+CHAT_LOCK = threading.RLock()
+USER_LOCK = threading.RLock()
+
+
+def chat_should_report(chat_id: Union[str, int]) -> bool:
+    try:
+        chat_setting = SESSION.query(ReportingChatSettings).get(str(chat_id))
+        if chat_setting:
+            return chat_setting.should_report
+        return True
+    finally:
+        SESSION.close()
+
+
+def user_should_report(user_id: int) -> bool:
+    try:
+        user_setting = SESSION.query(ReportingUserSettings).get(user_id)
+        if user_setting:
+            return user_setting.should_report
+        return True
+    finally:
+        SESSION.close()
+
+
+def set_chat_setting(chat_id: Union[int, str], setting: bool):
+    with CHAT_LOCK:
+        chat_setting = SESSION.query(ReportingChatSettings).get(str(chat_id))
+        if not chat_setting:
+            chat_setting = ReportingChatSettings(chat_id)
+
+        chat_setting.should_report = setting
+        SESSION.add(chat_setting)
+        SESSION.commit()
+
+
+def set_user_setting(user_id: int, setting: bool):
+    with USER_LOCK:
+        user_setting = SESSION.query(ReportingUserSettings).get(user_id)
+        if not user_setting:
+            user_setting = ReportingUserSettings(user_id)
+
+        user_setting.should_report = setting
+        SESSION.add(user_setting)
+        SESSION.commit()
+
+
+def migrate_chat(old_chat_id, new_chat_id):
+    with CHAT_LOCK:
+        chat_notes = SESSION.query(ReportingChatSettings).filter(
+            ReportingChatSettings.chat_id == str(old_chat_id)).all()
+        for note in chat_notes:
+            note.chat_id = str(new_chat_id)
+        SESSION.commit()
