f8593529f0367b4a3f6e1a2aef69c6a22c896094
Paul Larsen <pn1315@ic.ac.uk>
2018-02-24 12:24:41 +0000

    Ensure that commands are parsed correctly.

--- tg_bot/modules/helper_funcs/handlers.py
+++ tg_bot/modules/helper_funcs/handlers.py
@@ -8,18 +8,22 @@ class CustomCommandHandler(tg.CommandHandler):
                 and (update.message or update.edited_message and self.allow_edited)):
             message = update.message or update.edited_message
 
-            if message.text and len(message.text) > 1 and any(message.text.startswith(start) for start in ('/', '!')):
-                command = message.text[1:].split(None, 1)[0].split('@')
-                command.append(message.bot.username)  # in case the command was send without a username
-                if self.filters is None:
-                    res = True
-                elif isinstance(self.filters, list):
-                    res = any(func(message) for func in self.filters)
-                else:
-                    res = self.filters(message)
+            if message.text and len(message.text) > 1:
+                fst_word = message.text.split(None, 1)[0]
+                if len(fst_word) > 1 and any(fst_word.startswith(start) for start in ('/', '!')):
+                    command = fst_word[1:].split('@')
+                    command.append(message.bot.username)  # in case the command was sent without a username
+                    if self.filters is None:
+                        res = True
+                    elif isinstance(self.filters, list):
+                        res = any(func(message) for func in self.filters)
+                    else:
+                        res = self.filters(message)
 
-                return res and (command[0].lower() in self.command
-                                and command[1].lower() == message.bot.username.lower())
+                    return res and (command[0].lower() in self.command
+                                    and command[1].lower() == message.bot.username.lower())
+                else:
+                    return False
             else:
                 return False
 
