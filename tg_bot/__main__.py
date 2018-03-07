7fa8cbe53ea5047d8b613213013f9c23f3ed4a7c
Paul Larsen <pn1315@ic.ac.uk>
2018-03-07 16:52:49 +0000

    Help message is parsed with markdown

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -197,12 +197,14 @@ def help_button(bot: Bot, update: Update):
         elif prev_match:
             curr_page = int(prev_match.group(1))
             query.message.reply_text(HELP_STRINGS,
+                                     parse_mode=ParseMode.MARKDOWN,
                                      reply_markup=InlineKeyboardMarkup(
                                          paginate_modules(curr_page - 1, HELPABLE, "help")))
 
         elif next_match:
             next_page = int(next_match.group(1))
             query.message.reply_text(HELP_STRINGS,
+                                     parse_mode=ParseMode.MARKDOWN,
                                      reply_markup=InlineKeyboardMarkup(
                                          paginate_modules(next_page + 1, HELPABLE, "help")))
 
