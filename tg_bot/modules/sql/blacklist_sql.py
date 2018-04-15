30e09c8bf7c18c7c8a32977579ee3578f9cef507
Paul Larsen <pn1315@ic.ac.uk>
2018-04-15 23:04:04 +0100

    Fix a bug where new blacklist triggers wouldnt be active until the bot
    was reloaded for new chats

--- tg_bot/modules/sql/blacklist_sql.py
+++ tg_bot/modules/sql/blacklist_sql.py
@@ -36,7 +36,7 @@ def add_to_blacklist(chat_id, trigger):
 
         SESSION.merge(blacklist_filt)  # merge to avoid duplicate key issues
         SESSION.commit()
-        CHAT_BLACKLISTS.get(str(chat_id), set()).add(trigger)
+        CHAT_BLACKLISTS.setdefault(str(chat_id), set()).add(trigger)
 
 
 def rm_from_blacklist(chat_id, trigger):
