b2be9836a12a24abaf3fd15ac89e9023fb67f0a2
Anirudh Gupta <anirudhgupta109@gmail.com>
2018-04-28 07:06:36 +0000

    add a `/tempmute` command for muting users for a certain amount of time.
    Ported over from commit for tempban https://github.com/PaulSonOfLars/tgbot/commit/63de3571bb3139dfa4ecf8447082b3f68a8e3f94
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- tg_bot/modules/muting.py
+++ tg_bot/modules/muting.py
@@ -1,4 +1,5 @@
 import html
+import time
 from typing import Optional, List
 
 from telegram import Message, Chat, Update, Bot, User
@@ -8,7 +9,7 @@ from telegram.utils.helpers import mention_html
 
 from tg_bot import dispatcher
 from tg_bot.modules.helper_funcs.chat_status import bot_admin, user_admin, is_user_admin
-from tg_bot.modules.helper_funcs.extraction import extract_user
+from tg_bot.modules.helper_funcs.extraction import extract_user, extract_user_and_text
 from tg_bot.modules.log_channel import loggable
 
 
@@ -58,6 +59,105 @@ def mute(bot: Bot, update: Update, args: List[str]) -> str:
 @bot_admin
 @user_admin
 @loggable
+def temp_mute(bot: Bot, update: Update, args: List[str]) -> str:
+    chat = update.effective_chat  # type: Optional[Chat]
+    user = update.effective_user  # type: Optional[User]
+    message = update.effective_message  # type: Optional[Message]
+
+    user_id, reason = extract_user_and_text(message, args)
+
+    if not user_id:
+        message.reply_text("You don't seem to be referring to a user.")
+        return ""
+
+    try:
+        member = chat.get_member(user_id)
+    except BadRequest as excp:
+        if excp.message == "User not found":
+            message.reply_text("I can't seem to find this user")
+            return ""
+        else:
+            raise
+
+    if is_user_admin(chat, user_id, member):
+        message.reply_text("Afraid I can't stop an admin from talking!")
+        return ""
+
+    if user_id == bot.id:
+        update.effective_message.reply_text("I'm not muting myself!")
+        return ""
+
+    split_reason = reason.split(None, 1)
+    if not reason:
+        message.reply_text("You haven't specified a time to mute this user for!")
+        return ""
+
+    else:
+        time_val = split_reason[0].lower()
+        if len(split_reason) > 1:
+            reason = split_reason[1]
+        else:
+            reason = ""
+
+    if any(time_val.endswith(unit) for unit in ('m', 'h', 'd')):
+        unit = time_val[-1]
+        time_num = time_val[:-1]
+        if not time_num.isdigit():
+            message.reply_text("Invalid time amount specified.")
+            return ""
+
+        if unit == 'm':
+            mutetime = int(time.time() + int(time_num) * 60)
+        elif unit == 'h':
+            mutetime = int(time.time() + int(time_num) * 60 * 60)
+        elif unit == 'd':
+            mutetime = int(time.time() + int(time_num) * 24 * 60 * 60)
+        else:
+            # if it gets to this stage, goodluck debugging cause I definitely can't
+            return ""
+
+    else:
+        message.reply_text("Invalid time type specified. Expected m,h, or d, got: {}".format(time_val[-1]))
+        return ""
+
+    log = "<b>{}:</b>" \
+          "\n#TEMP MUTED" \
+          "\n<b>Admin:</b> {}" \
+          "\n<b>User:</b> {}" \
+          "\n<b>Time:</b> {}".format(html.escape(chat.title), mention_html(user.id, user.first_name),
+                                     mention_html(member.user.id, member.user.first_name), time_val)
+    if reason:
+        log += "\n<b>Reason:</b> {}".format(reason)
+
+    try:
+        if member:
+            if member.can_send_messages is None or member.can_send_messages:
+                bot.restrict_chat_member(chat.id, user_id, until_date=mutetime, can_send_messages=False)
+                message.reply_text("Muted! User will be muted for {}.".format(time_val))
+                return log
+
+            else:
+                message.reply_text("This user is already muted!")
+        else:
+            message.reply_text("This user isn't in the chat!")
+
+    except BadRequest as excp:
+        if excp.message == "Reply message not found":
+            message.reply_text("Muted! User will be muted for {}.".format(time_val), quote=False)
+            return log
+        else:
+            LOGGER.warning(update)
+            LOGGER.exception("ERROR muting user %s in chat %s (%s) due to %s", user_id, chat.title, chat.id,
+                             excp.message)
+            message.reply_text("Guess the user can keep babbling since I wasn't able to mute them")
+
+    return ""
+
+
+@run_async
+@bot_admin
+@user_admin
+@loggable
 def unmute(bot: Bot, update: Update, args: List[str]) -> str:
     chat = update.effective_chat  # type: Optional[Chat]
     user = update.effective_user  # type: Optional[User]
@@ -98,12 +198,15 @@ __help__ = """
 *Admin only:*
  - /mute <userhandle>: silences a user. Can also be used as a reply, muting the replied to user.
  - /unmute <userhandle>: unmutes a user. Can also be used as a reply, muting the replied to user.
+ - /tmute <userhandle> x(m/h/d): mutes a user for x time. (via handle, or reply). m = minutes, h = hours, d = days.
 """
 
 __mod_name__ = "Muting"
 
 MUTE_HANDLER = CommandHandler("mute", mute, pass_args=True, filters=Filters.group)
+TEMPMUTE_HANDLER = CommandHandler(["tmute", "tempmute"], temp_mute, pass_args=True, filters=Filters.group)
 UNMUTE_HANDLER = CommandHandler("unmute", unmute, pass_args=True, filters=Filters.group)
 
 dispatcher.add_handler(MUTE_HANDLER)
+dispatcher.add_handler(TEMPMUTE_HANDLER)
 dispatcher.add_handler(UNMUTE_HANDLER)
