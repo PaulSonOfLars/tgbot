b4231d91a6ff811f858424d2b83cceed528092eb
Maverick <maver_ck@outlook.com>
2018-05-04 15:59:20 +0800

    reverted stickers
    Signed-off-by: Maverick <maver_ck@outlook.com>

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -55,7 +55,7 @@ if ENV:
     DEL_CMDS = bool(os.environ.get('DEL_CMDS', False))
     STRICT_GBAN = bool(os.environ.get('STRICT_GBAN', False))
     WORKERS = int(os.environ.get('WORKERS', 8))
-    BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADBAADDgsAArIFDgABxwpNGVulFgsC')
+    BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
 
 else:
