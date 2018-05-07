efa406097fae482c2a8758a31b689ebbe1d59111
Maverick <maver_ck@outlook.com>
2018-05-07 22:38:38 +0800

    amendments for start text

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,11 +18,11 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hi {}, my name is {}! If you have any questions on how to use me, read /help - and then head to @MarieSupport.
+Hi {}, my name is {}! If you have any questions on how to use me, read /help for more details.
 
 I'm a group manager bot maintained by [this wonderful person](tg://user?id={}). I'm built in python3, using the \
 python-telegram-bot library, and am fully opensource - you can find what makes me tick \
-[here](github.com/1maverick1/tgbot)! Here is a link to the [original source](github.com/PaulSonOfLars/tgbot).
+[here](github.com/1maverick1/tgbot)! Here is a link to the [original sources](github.com/PaulSonOfLars/tgbot).
 
 You can find the list of available commands with /help.
 
