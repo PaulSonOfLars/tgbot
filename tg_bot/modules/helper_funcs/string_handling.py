7c276b3f977dbcc6f50865714edfea85a48585c8
Maverick <maver_ck@outlook.com>
2018-05-07 18:11:06 +0800

    support buttonurl escaping

--- tg_bot/modules/helper_funcs/string_handling.py
+++ tg_bot/modules/helper_funcs/string_handling.py
@@ -19,7 +19,7 @@ MATCH_MD = re.compile(r'\*(.*?)\*|'
 
 # regex to find []() links -> hyperlinks/buttons
 LINK_REGEX = re.compile(r'(?<!\\)\[.+?\]\((.*?)\)')
-BTN_URL_REGEX = re.compile(r"(?<!\\)(\[([^\[]+?)\]\(buttonurl:(?:/{0,2})(.+?)(:same)?\))")
+BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\(buttonurl:(?:/{0,2})(.+?)(:same)?\))")
 
 
 def _selective_escape(to_parse: str) -> str:
@@ -120,10 +120,23 @@ def button_markdown_parser(txt: str, entities: Dict[MessageEntity, str] = None, 
     note_data = ""
     buttons = []
     for match in BTN_URL_REGEX.finditer(markdown_note):
-        # create a thruple with button label, url, and newline status
-        buttons.append((match.group(2), match.group(3), bool(match.group(4))))
-        note_data += markdown_note[prev:match.start(1)]
-        prev = match.end(1)
+        # Check if btnurl is escaped
+        n_escapes = 0
+        to_check = match.start(1) - 1
+        while to_check > 0 and markdown_note[to_check] == "\\":
+            n_escapes += 1
+            to_check -= 1
+
+        # if even, not escaped -> create button
+        if n_escapes % 2 == 0:
+            # create a thruple with button label, url, and newline status
+            buttons.append((match.group(2), match.group(3), bool(match.group(4))))
+            note_data += markdown_note[prev:match.start(1)]
+            prev = match.end(1)
+        # if odd, escaped -> move along
+        else:
+            note_data += markdown_note[prev:to_check]
+            prev = match.start(1) - 1
     else:
         note_data += markdown_note[prev:]
 
