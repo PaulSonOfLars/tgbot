150bc322ed72effc880eef96e890e5ba900ab5b4
Paul Larsen <pn1315@ic.ac.uk>
2018-05-07 13:07:18 +0100

    update sample config

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -18,7 +18,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
     MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
     LOAD = []
-    NO_LOAD = ['translation']
+    NO_LOAD = ['translation', 'rss']
     WEBHOOK = False
     URL = None
 
