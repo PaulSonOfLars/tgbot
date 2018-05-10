f44b2df1bb7a2e074c2ed453ead8e659b08049c4
Maverick <maver_ck@outlook.com>
2018-05-11 06:59:49 +0800

    revert webhook

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -431,7 +431,7 @@ def main():
 
     if WEBHOOK:
         LOGGER.info("Using webhooks.")
-        updater.start_webhook(listen="0.0.0.0",
+        updater.start_webhook(listen="127.0.0.1",
                               port=PORT,
                               url_path=TOKEN)
 
