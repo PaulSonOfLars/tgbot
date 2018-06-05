84ab5880d238edfba3d47dbe6c28d13a3cce67df
Maverick <maver_ck@outlook.com>
2018-06-06 03:20:46 +0800

    warn and warn_ban counts for channel log (#23)
    *  warn and warn_ban counts for channel log
    
    Channel log with warn and warn_ban counts

--- tg_bot/modules/warns.py
+++ tg_bot/modules/warns.py
@@ -56,9 +56,11 @@ def warn(user: User, chat: Chat, reason: str, message: Message, warner: User = N
                      "\n#WARN_BAN" \
                      "\n<b>Admin:</b> {}" \
                      "\n<b>User:</b> {}" \
-                     "\n<b>Reason:</b> {}".format(html.escape(chat.title),
-                                                  warner_tag,
-                                                  mention_html(user.id, user.first_name), reason)
+                     "\n<b>Reason:</b> {}"\
+                     "\n<b>Counts:</b> <code>{}/{}</code>".format(html.escape(chat.title),
+                                                                  warner_tag,
+                                                                  mention_html(user.id, user.first_name), 
+                                                                  reason, num_warns, limit)
 
     else:
         keyboard = InlineKeyboardMarkup(
@@ -73,9 +75,11 @@ def warn(user: User, chat: Chat, reason: str, message: Message, warner: User = N
                      "\n#WARN" \
                      "\n<b>Admin:</b> {}" \
                      "\n<b>User:</b> {}" \
-                     "\n<b>Reason:</b> {}".format(html.escape(chat.title),
-                                                  warner_tag,
-                                                  mention_html(user.id, user.first_name), reason)
+                     "\n<b>Reason:</b> {}"\
+                     "\n<b>Counts:</b> <code>{}/{}</code>".format(html.escape(chat.title),
+                                                                  warner_tag,
+                                                                  mention_html(user.id, user.first_name), 
+                                                                  reason, num_warns, limit)
 
     try:
         message.reply_text(reply, reply_markup=keyboard, parse_mode=ParseMode.HTML)
