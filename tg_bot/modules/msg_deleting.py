a31711dad7ab760692532d5ab06ea0982b272583
Shabier <32566872+Shabier@users.noreply.github.com>
2018-06-11 17:50:32 +0200

    Confirmation is unnecessary
    The confirmation takes another couple actions to delete (which counters the idea of mass deleting)

--- tg_bot/modules/msg_deleting.py
+++ tg_bot/modules/msg_deleting.py
@@ -47,7 +47,6 @@ def purge(bot: Bot, update: Update, args: List[str]) -> str:
                 elif err.message != "Message to delete not found":
                     LOGGER.exception("Error while purging chat messages.")
 
-            bot.send_message(chat.id, "Purge complete.")
             return "<b>{}:</b>" \
                    "\n#PURGE" \
                    "\n<b>Admin:</b> {}" \
