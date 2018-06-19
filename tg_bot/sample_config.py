c57cc1857892a3f1c6f3b7df011c207b5aeb9919
Maverick <maver_ck@outlook.com>
2018-06-19 20:43:04 +0800

    config updated for gmute

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -32,6 +32,7 @@ class Config(object):
     PORT = 5000
     DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
     STRICT_GBAN = False
+    STRICT_GMUTE = False
     WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
     BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
     ALLOW_EXCL = False  # Allow ! commands as well as /
