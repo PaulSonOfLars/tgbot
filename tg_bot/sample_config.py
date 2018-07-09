db6f37a41121f7e1e490708ec559633f322ad362
kbprashant <41023256+kbprashant@users.noreply.github.com>
2018-07-10 01:23:10 +0530

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -10,9 +10,9 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = "567660987:AAHqIyhOUBpE7CTr1T9TjAFNCyMD4kKTxFw"
+    OWNER_ID = "500190041"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "prashant_b"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
