1603334636c562e29c1a06861b60b60c06982cc6
Skittles9823 <skat1@live.com.au>
2018-07-26 20:51:03 +1000

    Add /rmjoin command `alter table welcome_pref add column del_joined bigint;` Be sure to update your databases if you pick this.
    # Conflicts:
    #	tg_bot/modules/welcome.py

--- tg_bot/modules/welcome.py
+++ tg_bot/modules/welcome.py
@@ -9,7 +9,7 @@ from telegram.utils.helpers import mention_markdown, mention_html, escape_markdo
 
 import tg_bot.modules.sql.welcome_sql as sql
 from tg_bot import dispatcher, OWNER_ID, LOGGER
-from tg_bot.modules.helper_funcs.chat_status import user_admin
+from tg_bot.modules.helper_funcs.chat_status import user_admin, can_delete
 from tg_bot.modules.helper_funcs.misc import build_keyboard, revert_buttons
 from tg_bot.modules.helper_funcs.msg_types import get_welcome_type
 from tg_bot.modules.helper_funcs.string_handling import markdown_parser, \
@@ -129,7 +129,7 @@ def new_member(bot: Bot, update: Update):
 
                 sent = send(update, res, keyboard,
                             sql.DEFAULT_WELCOME.format(first=first_name))  # type: Optional[Message]
-            del_message = sql.get_del_pref(chat.id)
+            delete_join(bot, update)
 
 
         prev_welc = sql.get_clean_pref(chat.id)
