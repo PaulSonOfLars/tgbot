98f3e29c1a37a58531ad4f3d58bca9680bed5cb1
Maverick <maver_ck@outlook.com>
2018-08-02 02:12:00 +0800

    fixed loop allowing multilinks

--- tg_bot/modules/getlink.py
+++ tg_bot/modules/getlink.py
@@ -16,13 +16,19 @@ def getlink(bot: Bot, update: Update, args: List[int]):
         chat_id = int(args[0])
     else:
         update.effective_message.reply_text("You don't seem to be referring to a chat")
-    chat = bot.get_chat(chat_id)
-    bot_member = chat.get_member(bot.id)
-    if bot_member.can_invite_users:
-        invitelink = bot.exportChatInviteLink(chat_id)
-        update.effective_message.reply_text(invitelink)
-    else:
-        update.effective_message.reply_text("I don't have access to the invite link!")
+    for chat_id in args:
+        try:
+            chat = bot.getChat(chat_id)
+            bot_member = chat.get_member(bot.id)
+            if bot_member.can_invite_users:
+                invitelink = bot.exportChatInviteLink(chat_id)
+                update.effective_message.reply_text("Invite link for: " + chat_id + "\n" + invitelink)
+            else:
+                update.effective_message.reply_text("I don't have access to the invite link.")
+        except BadRequest as excp:
+                update.effective_message.reply_text(excp.message + " " + str(chat_id))
+        except TelegramError as excp:
+                update.effective_message.reply_text(excp.message + " " + str(chat_id))
 
 GETLINK_HANDLER = CommandHandler("getlink", getlink, pass_args=True, filters=CustomFilters.sudo_filter)
 
