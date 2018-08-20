438c6cefe9cdf7a871ed91156e1c33e712004c64
Shabier <32566872+Shabier@users.noreply.github.com>
2018-08-20 22:47:08 +0200

    [Fixed] Lyrics module

--- tg_bot/modules/lyrics.py
+++ tg_bot/modules/lyrics.py
@@ -1,6 +1,7 @@
 from PyLyrics import *
 from telegram import Update, Bot
 from telegram.ext import run_async
+from typing import Optional, List
 
 from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot import dispatcher
@@ -10,7 +11,7 @@ from requests import get
 LYRICSINFO = "\n[Full Lyrics](http://lyrics.wikia.com/wiki/%s:%s)"
 
 @run_async
-def lyrics(Bot, update: Update, user, args):
+def lyrics(bot: Bot, update: Update, args: List[str]):
     message = update.effective_message
     text = message.text[len('/lyrics '):]
     song = " ".join(args).split("- ")
@@ -29,13 +30,13 @@ def lyrics(Bot, update: Update, user, args):
             lyrics = "\n".join(PyLyrics.getLyrics(
                 song[0], song[1]).split("\n")[:20])
         except ValueError as e:
-            return core.message("❌ Song %s not found :(" % song[1], failed=True)
+            return update.effective_message.reply_text("Song %s not found :(" % song[1], failed=True)
         else:
             lyricstext = LYRICSINFO % (song[0].replace(
                 " ", "_"), song[1].replace(" ", "_"))
-            return core.message(lyrics + lyricstext, parse_mode="MARKDOWN")
+            return update.effective_message.reply_text(lyrics + lyricstext, parse_mode="MARKDOWN")
     else:
-        return core.message("Invalid syntax!", failed=True)
+        return update.effective_message.reply_text("Invalid syntax- try Artist - Title!", failed=True)
 
 
 __help__ = """
@@ -44,6 +45,6 @@ __help__ = """
 
 __mod_name__ = "Lyrics"
 
-lyrics_handle = DisableAbleCommandHandler("lyrics", lyrics)
+LYRICS_HANDLER = DisableAbleCommandHandler("lyrics", lyrics, pass_args=True)
 
-dispatcher.add_handler(lyrics_handle)
+dispatcher.add_handler(LYRICS_HANDLER)
