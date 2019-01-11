54f1513b208ef0ad177ec05b3d28b4948c317d39
Paul Larsen <pn1315@ic.ac.uk>
2019-01-11 17:30:35 +0000

    remove an incorrect string

--- tg_bot/modules/locks.py
+++ tg_bot/modules/locks.py
@@ -299,7 +299,7 @@ __help__ = """
 
 Locks can be used to restrict a group's users.
 eg:
-Locking urls will auto-delete all messages with urls which haven't been whitelisted, locking stickers will delete all \
+Locking urls will auto-delete all messages with urls, locking stickers will delete all \
 stickers, etc.
 Locking bots will stop non-admins from adding bots to the chat.
 """
