70ccf9ec64cddc1f530783525347bbaad426ee7d
Netfreak25 <moritz.kuhn@gmail.com>
2019-02-02 21:27:33 +0100

    added sql base
    added sql base for adding channel

---
+++ tg_bot/modules/sql/addchannel.py
@@ -0,0 +1,20 @@
+import threading
+
+from sqlalchemy import Column, Integer, UnicodeText, String, ForeignKey, UniqueConstraint, func
+
+from tg_bot import dispatcher
+from tg_bot.modules.sql import BASE, SESSION
+
+class Channels(BASE):
+    __tablename__ = "channels"
+    chat_id = Column(String(14), primary_key=True)
+    chat_name = Column(UnicodeText, nullable=False)
+
+    def __init__(self, chat_id, chat_name):
+        self.chat_id = str(chat_id)
+        self.chat_name = chat_name
+
+    def __repr__(self):
+
+
+Chats.__table__.create(checkfirst=True)
