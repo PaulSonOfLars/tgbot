665e5e5919a105399a544af0385d9adcc336f782
Netfreak25 <moritz.kuhn@gmail.com>
2019-02-02 22:17:37 +0100

    added get_all_channels
    ...

--- tg_bot/modules/sql/users_sql.py
+++ tg_bot/modules/sql/users_sql.py
@@ -131,6 +131,13 @@ def get_all_chats():
         SESSION.close()
 
 
+def get_all_channels():
+    try:
+        return SESSION.query(Channels).all()
+    finally:
+        SESSION.close()
+
+
 def get_user_num_chats(user_id):
     try:
         return SESSION.query(ChatMembers).filter(ChatMembers.user == int(user_id)).count()
