381cac244771a1fafd876b017eecec7cce2d1b2e
Netfreak25 <moritz.kuhn@gmail.com>
2019-02-03 19:17:48 +0100

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -24,7 +24,8 @@ PM_START_TEXT = """
 Hi {}, my name is {}! I am a private modification of the group management bot Marie!
 This modification focuses on expanding the gban feature to channels!
 
-@yeah
+The original Bot can be found @BanhammerMarie_bot
+
 You can find the list of available commands with /help.
 """
 
