39e50323b7f0ca35c9e3d881385a497edf38bbe6
Netfreak25 <moritz.kuhn@gmail.com>
2019-02-03 01:55:45 +0100

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -432,7 +432,7 @@ def main():
     migrate_handler = MessageHandler(Filters.status_update.migrate, migrate_chats)
 
 
-    test_handler = MessageHandler(Filters.all, testf, edited_channel_post=True
+    test_handler = MessageHandler(Filters.all, testf, edited_channel_post=True)
 
     # dispatcher.add_handler(test_handler)
     dispatcher.add_handler(test_handler)
