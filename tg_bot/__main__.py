1ff3fdc2c44f22a7035940c84db1d1970cb71393
Netfreak25 <moritz.kuhn@gmail.com>
2019-02-03 02:08:26 +0100

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -463,7 +463,7 @@ def main():
 
     else:
         LOGGER.info("Using long polling.")
-        updater.start_polling(timeout=15, read_latency=4)
+        updater.start_polling(timeout=15, read_latency=4, allowed_updates="")
 
     updater.idle()
 
