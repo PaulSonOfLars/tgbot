beb5d4b75766dde7116f6a202b26d9612dd968ae
Netfreak25 <moritz.kuhn@gmail.com>
2019-02-03 14:13:04 +0100

    removed donation info

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -44,7 +44,6 @@ the things I can help you with.
  - /start: start the bot
  - /help: PM's you this message.
  - /help <module name>: PM's you info about that module.
- - /donate: information about how to donate!
  - /settings:
    - in PM: will send you your settings for all supported modules.
    - in a group: will redirect you to pm, with all that chat's settings.
