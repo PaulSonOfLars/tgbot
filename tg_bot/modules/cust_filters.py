9733977b9056f05fd46d5fb2efdde7ab961caaa3
Netfreak25 <moritz.kuhn@gmail.com>
2019-02-03 21:31:31 +0100

    Update cust_filters.py

--- tg_bot/modules/cust_filters.py
+++ tg_bot/modules/cust_filters.py
@@ -220,7 +220,7 @@ doin?
  - /stop <filter keyword>: stop that filter.
 """
 
-__mod_name__ = "Codeword Reply Filters"
+__mod_name__ = "Codeword Filters"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
