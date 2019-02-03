fa487fadff610d1656288920c8576eaf33a44c6a
Netfreak25 <moritz.kuhn@gmail.com>
2019-02-03 14:00:10 +0100

    changed names of modules

--- tg_bot/modules/global_bans.py
+++ tg_bot/modules/global_bans.py
@@ -309,7 +309,7 @@ you and your groups by removing spam flooders as quickly as possible. They can b
 /gbanstat
 """
 
-__mod_name__ = "Global Bans"
+__mod_name__ = "Bans (Global)"
 
 GBAN_HANDLER = CommandHandler("gban", gban, pass_args=True,
                               filters=CustomFilters.sudo_filter | CustomFilters.support_filter)
