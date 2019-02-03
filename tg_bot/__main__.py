0f223bd522a97c24f6d82c9235c68915b7dfeb43
Netfreak25 <moritz.kuhn@gmail.com>
2019-02-03 14:32:36 +0100

    less text

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -37,7 +37,6 @@ the things I can help you with.
 *Main* commands available:
  - /start: start the bot
  - /help: PM's you this message.
- - /help <module name>: PM's you info about that module.
  - /settings:
    - in PM: will send you your settings for all supported modules.
    - in a group: will redirect you to pm, with all that chat's settings.
