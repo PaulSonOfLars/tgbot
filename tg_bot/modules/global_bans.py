c03885fa6fe54e7497ae9a343b4c8ba63b806e21
Netfreak25 <moritz.kuhn@gmail.com>
2019-02-03 14:22:09 +0100

    more text

--- tg_bot/modules/global_bans.py
+++ tg_bot/modules/global_bans.py
@@ -303,7 +303,9 @@ def __chat_settings__(chat_id, user_id):
 __help__ = """
 *Admin only:*
  - /gbanstat <on/off/yes/no>: Will disable the effect of global bans on your group, or return your current settings.
-
+ - /gban <userhandle> bans a user. (via handle, or reply)
+ - /ungban <userhandle> bans a user. (via handle, or reply)
+ 
 Gbans, also known as global bans, are used by the bot owners to ban spammers across all *groups*. This helps protect \
 you and your groups by removing spam flooders as quickly as possible. They can be disabled for you group by calling \
 /gbanstat
