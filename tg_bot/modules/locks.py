d11c54e4bfa3ddfa343d89c858252f9a0b3a1762
Netfreak25 <moritz.kuhn@gmail.com>
2019-02-06 18:13:08 +0100

    Update locks.py

--- tg_bot/modules/locks.py
+++ tg_bot/modules/locks.py
@@ -79,7 +79,7 @@ def send(update, message, keyboard):
 
 
 def send_lock_msg(bot: Bot, update: Update):
-    user_id = update.effective_message
+    user_id = update.effective_message.from.id
     print(user_id)
    # if user_id:
    #     slapped_user = bot.get_chat(user_id)
