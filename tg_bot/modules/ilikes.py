6519430213da4f6bceb72831afa11cd6c2c154e7
Netfreak25 <moritz.kuhn@gmail.com>
2019-02-07 20:47:11 +0100

    Update ilikes.py

--- tg_bot/modules/ilikes.py
+++ tg_bot/modules/ilikes.py
@@ -65,6 +65,7 @@ def rest_handler(bot: Bot, update: Update):
     chat = update.effective_chat  # type: Optional[Chat]
     if Filters.location(msg):
         print("location found")
+        get_like_buttons(bot, update)
 
 
 @run_async
