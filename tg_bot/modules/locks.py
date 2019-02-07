24bbfb13e5c8358ce2ecc538f66ba206c5a7d885
Netfreak25 <moritz.kuhn@gmail.com>
2019-02-07 15:28:17 +0100

    Update locks.py

--- tg_bot/modules/locks.py
+++ tg_bot/modules/locks.py
@@ -275,11 +275,6 @@ def del_lockables(bot: Bot, update: Update):
 def rest_handler(bot: Bot, update: Update):
     msg = update.effective_message  # type: Optional[Message]
     chat = update.effective_chat  # type: Optional[Chat]
-    
-    print(msg)
-    print(filter(msg))
-
-
     for restriction, filter in RESTRICTION_TYPES.items():
         if filter(msg) and sql.is_restr_locked(chat.id, restriction) and can_delete(chat, bot.id):
             try:
