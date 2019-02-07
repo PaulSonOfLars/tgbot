0d9906937860f432cc5f50d2a49ec22e44dc5735
Netfreak25 <moritz.kuhn@gmail.com>
2019-02-07 19:15:07 +0100

    Update ilikes_sql.py

--- tg_bot/modules/sql/ilikes_sql.py
+++ tg_bot/modules/sql/ilikes_sql.py
@@ -9,17 +9,14 @@ from tg_bot.modules.sql import SESSION, BASE
 
 class iLikes(BASE):
     __tablename__ = "ilikes"
-    chat_id = Column(String(14), primary_key=True)
-    msg_id = Column(String(14), nullable=False)
+    ilikes_id = Column(String(100), primary_key=True)
     found = Column(Integer, nullable=False)
     thanks = Column(Integer, nullable=False)
     notfound = Column(Integer, nullable=False)
     timestamp = Column(String(50), nullable=False)
 
-    def __init__(self, chat_id, msg_id):
-        self.chat_id = str(chat_id)
-        self.msg_id = str(msg_id)
-
+    def __init__(self, ilikes_id):
+        self.ilikes_id = str(chat_id)
         self.found = 0
         self.thanks = 0
         self.notfound = 0
@@ -32,16 +29,13 @@ class iLikes(BASE):
 
 class iLikes_Clicks(BASE):
     __tablename__ = "ilikes_clicks"
-    ilikes_id = Column(String(14), primary_key=True)
-    user_id = Column(String(14), nullable=False)
+    ilikes_click_id = Column(String(100), primary_key=True)
     found = Column(Integer, nullable=False)
     thanks = Column(Integer, nullable=False)
     notfound = Column(Integer, nullable=False)
 
-    def __init__(self, ilikes_id, user_id, found, thanks, notfound):
-        self.ilikes_id = str(ilikes_id)
-        self.user_id = str(user_id)
-
+    def __init__(self, ilikes_click_id, user_id, found, thanks, notfound):
+        self.ilikes_click_id = str(ilikes_id)
         self.found = str(found)
         self.thanks = str(thanks)
         self.notfound = str(notfound)
@@ -66,10 +60,10 @@ INSERTION_LOCK = threading.RLock()
 
 def add_iLike(chat_id, msg_id):
     with INSERTION_LOCK:
-        ilikes_id = SESSION.query(iLikes).filter(iLikes.chat_id == str(chat_id),
-	                                             iLikes.msg_id == str(msg_id),).all()
+    	new_ilikes_id = str(chat_id)+str(msg_id)
+        ilikes_id = SESSION.query(iLikes).filter(iLikes.chat_id == new_ilikes_id).all()
         if not ilikes_id:
-            ilikes_id = iLikes(chat_id, msg_id)
+            ilikes_id = iLikes(new_ilikes_id)
             SESSION.add(ilikes_id)
             SESSION.commit()
             return True
