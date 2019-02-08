473e670f20f0aba4586c6645abef39cae3961c0e
Netfreak25 <moritz.kuhn@gmail.com>
2019-02-08 21:53:04 +0100

    Update ilikes.py

--- tg_bot/modules/ilikes.py
+++ tg_bot/modules/ilikes.py
@@ -417,11 +417,12 @@ This module sends like Buttons
 __mod_name__ = "iLikes"
 dispatcher.add_handler(MessageHandler(Filters.location & Filters.group, location_handler), 2)
 
-settings_callback_handler = CallbackQueryHandler(thank_button, pattern=r"thanks_")
-
+settings_callback_handler = CallbackQueryHandler(thank_button,
+                           filters=CustomFilters.sudo_filter | CustomFilters.support_filter | Filters.group, pattern=r"thanks_")
 toggle_handler = CommandHandler("iLikes", toggle_ilikes,
-                           filters=CustomFilters.sudo_filter | CustomFilters.support_filter, pass_args=True)
-settings_handler = CommandHandler("ilike", send_like_buttons, pass_args=True)
+                           filters=CustomFilters.sudo_filter | CustomFilters.support_filter | Filters.group, pass_args=True)
+settings_handler = CommandHandler("ilike", send_like_buttons,
+                           filters=CustomFilters.sudo_filter | CustomFilters.support_filter | Filters.group, pass_args=True)
 
 dispatcher.add_handler(toggle_handler)
 dispatcher.add_handler(settings_handler)
