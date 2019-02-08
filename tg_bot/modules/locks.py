97bf273f67cc99743725790f3a638010e6e16f09
Netfreak25 <moritz.kuhn@gmail.com>
2019-02-08 23:49:53 +0100

    Update locks.py

--- tg_bot/modules/locks.py
+++ tg_bot/modules/locks.py
@@ -17,6 +17,7 @@ from tg_bot.modules.helper_funcs.chat_status import can_delete, is_user_admin, u
 from tg_bot.modules.log_channel import loggable
 from tg_bot.modules.sql import users_sql
 from tg_bot.modules.helper_funcs.extraction import extract_user
+from tg_bot.modules.helper_funcs.filters import CustomFilters
 
 LOCK_TYPES = {'sticker': Filters.sticker,
               'audio': Filters.audio,
