3c48d3c475ef778a1da50d1b42c339f8c3fa4b4e
Netfreak25 <moritz.kuhn@gmail.com>
2019-02-09 00:39:10 +0100

    no welcome as default

--- tg_bot/modules/sql/welcome_sql.py
+++ tg_bot/modules/sql/welcome_sql.py
@@ -78,7 +78,7 @@ def get_welc_pref(chat_id):
         return welc.should_welcome, welc.custom_welcome, welc.welcome_type
     else:
         # Welcome by default.
-        return True, DEFAULT_WELCOME, Types.TEXT
+        return False, DEFAULT_WELCOME, Types.TEXT
 
 
 def get_gdbye_pref(chat_id):
@@ -88,7 +88,7 @@ def get_gdbye_pref(chat_id):
         return welc.should_goodbye, welc.custom_leave, welc.leave_type
     else:
         # Welcome by default.
-        return True, DEFAULT_GOODBYE, Types.TEXT
+        return False, DEFAULT_GOODBYE, Types.TEXT
 
 
 def set_clean_welcome(chat_id, clean_welcome):
