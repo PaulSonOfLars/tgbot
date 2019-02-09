91189c8728dce96d01ed8fe167eb000a8d2857ec
Netfreak25 <moritz.kuhn@gmail.com>
2019-02-09 16:35:27 +0100

    Update log_channel.py

--- tg_bot/modules/log_channel.py
+++ tg_bot/modules/log_channel.py
@@ -154,7 +154,7 @@ Setting the log channel is done by:
 - forwarding the /setlog to the group
 """
 
-    __mod_name__ = "Berichte"
+    __mod_name__ = "Protokoll Kanal"
 
     LOG_HANDLER = CommandHandler("logchannel", logging)
     SET_LOG_HANDLER = CommandHandler("setlog", setlog)
