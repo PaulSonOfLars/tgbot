8fa7da3a378cedf13ec225638ceac442a3734362
Netfreak25 <moritz.kuhn@gmail.com>
2019-02-09 16:36:48 +0100

    ..

--- tg_bot/modules/antiflood.py
+++ tg_bot/modules/antiflood.py
@@ -127,7 +127,7 @@ __help__ = """
  - /setflood <int/'no'/'off'>: enables or disables flood control
 """
 
-__mod_name__ = "Anti Spam"
+__mod_name__ = "Sicherheit - Spam"
 
 FLOOD_BAN_HANDLER = MessageHandler(Filters.all & ~Filters.status_update & Filters.group, check_flood)
 SET_FLOOD_HANDLER = CommandHandler("setflood", set_flood, pass_args=True, filters=Filters.group)
--- tg_bot/modules/disable.py
+++ tg_bot/modules/disable.py
@@ -146,7 +146,7 @@ if is_module_loaded(FILENAME):
         return build_curr_disabled(chat_id)
 
 
-    __mod_name__ = "Befehle deaktivieren"
+    __mod_name__ = "Sicherheit - Befehle"
 
     __help__ = """
  - /cmds: check the current status of disabled commands
