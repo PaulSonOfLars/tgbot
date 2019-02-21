a13534587e2838d56cedf9514acf51d961ef051b
Maverick <maver_ck@outlook.com>
2019-02-22 03:21:30 +0800

    new shout module

---
+++ tg_bot/modules/shout.py
@@ -0,0 +1,35 @@
+from telegram import Update, Bot
+from telegram.ext import run_async
+
+from tg_bot.modules.disable import DisableAbleCommandHandler
+from tg_bot import dispatcher
+
+@run_async
+def shout(bot: Bot, update: Update, args):
+    msg = "```"
+    text = " ".join(args)
+    result = []
+    result.append(' '.join([s for s in text]))
+    for pos, symbol in enumerate(text[1:]):
+        result.append(symbol + ' ' + '  ' * pos + symbol)
+    result = list("\n".join(result))
+    result[0] = text[0]
+    result = "".join(result)
+    msg = "```\n" + result + "```"
+    return update.effective_message.reply_text(msg, parse_mode="MARKDOWN")
+    
+__help__ = """
+ - /shout <keyword>: write anything you want to give loud shout.
+    ```
+    t e s t
+    e e
+    s   s
+    t     t
+    ```
+"""
+
+__mod_name__ = "Shout"
+
+SHOUT_HANDLER = DisableAbleCommandHandler("shout", shout, pass_args=True)
+
+dispatcher.add_handler(SHOUT_HANDLER)
