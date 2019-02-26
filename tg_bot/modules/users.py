0fe016eec1a23224715fdc2d6eae8649ac00ab3a
Maverick <maver_ck@outlook.com>
2019-02-27 05:31:50 +0800

    fix full stop sentence for stats

--- tg_bot/modules/users.py
+++ tg_bot/modules/users.py
@@ -107,7 +107,7 @@ def __user_info__(user_id):
 
 
 def __stats__():
-    return "{} users, across {} chats".format(sql.num_users(), sql.num_chats())
+    return "{} users, across {} chats.".format(sql.num_users(), sql.num_chats())
 
 
 def __gdpr__(user_id):
