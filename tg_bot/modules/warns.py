a986c52998b2f6493713b50fb7a4140dfb4efb5b
Maverick <maver_ck@outlook.com>
2019-03-17 07:33:33 +0800

    minor changes to warn help

--- tg_bot/modules/warns.py
+++ tg_bot/modules/warns.py
@@ -434,8 +434,8 @@ __help__ = """
  - /unwarn <userhandle>: same as /rmwarn
  - /addwarn <keyword> <reply message>: set a warning filter on a certain keyword. If you want your keyword to \
 be a sentence, encompass it with quotes, as such: `/addwarn "very angry" This is an angry user`. 
- - /nowarn <keyword>: stop a warning filter
- - /warnlimit <num>: set the warning limit
+ - /nowarn <keyword>: stop a warning filter.
+ - /warnlimit <num>: set the warning limit.
  - /strongwarn <on/yes/off/no>: If set to on, exceeding the warn limit will result in a ban. Else, will just kick.
 """
 
