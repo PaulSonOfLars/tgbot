acb8ccc6eab8ae2fb1721adcbd6e70dd85446593
Paul Larsen <pnvlarsen@gmail.com>
2019-04-17 16:07:35 +0100

    Fix counting issue with antiflood

--- tg_bot/modules/sql/antiflood_sql.py
+++ tg_bot/modules/sql/antiflood_sql.py
@@ -51,12 +51,12 @@ def update_flood(chat_id: str, user_id) -> bool:
         if limit == 0:  # no antiflood
             return False
 
+        count += 1
         if user_id != curr_user_id or user_id is None:  # other user
-            CHAT_FLOOD[str(chat_id)] = (user_id, DEF_COUNT, limit)
+            CHAT_FLOOD[str(chat_id)] = (user_id, count, limit)
             return False
 
-        count += 1
-        if count > limit:  # too many msgs, kick
+        if count >= limit:  # too many msgs, kick
             CHAT_FLOOD[str(chat_id)] = (None, DEF_COUNT, limit)
             return True
 
