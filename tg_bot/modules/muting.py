b99ca146a2b17e1375cd2efd0d352fa06bccc9a7
Mattia <mattiafailla@hotmail.it>
2019-07-08 12:14:19 +0200

    Traduzione modulo muting

--- tg_bot/modules/muting.py
+++ tg_bot/modules/muting.py
@@ -25,18 +25,18 @@ def mute(bot: Bot, update: Update, args: List[str]) -> str:
 
     user_id = extract_user(message, args)
     if not user_id:
-        message.reply_text("You'll need to either give me a username to mute, or reply to someone to be muted.")
+        message.reply_text("Dovrai o darmi un nome utente per mutarlo o rispondere a qualcuno che deve essere mutato.")
         return ""
 
     if user_id == bot.id:
-        message.reply_text("I'm not muting myself!")
+        message.reply_text("Non mi muterò!")
         return ""
 
     member = chat.get_member(int(user_id))
 
     if member:
         if is_user_admin(chat, user_id, member=member):
-            message.reply_text("Afraid I can't stop an admin from talking!")
+            message.reply_text("Ho paura di non poter impedire a un amministratore di parlare!")
 
         elif member.can_send_messages is None or member.can_send_messages:
             bot.restrict_chat_member(chat.id, user_id, can_send_messages=False)
@@ -49,9 +49,9 @@ def mute(bot: Bot, update: Update, args: List[str]) -> str:
                                               mention_html(member.user.id, member.user.first_name))
 
         else:
-            message.reply_text("This user is already muted!")
+            message.reply_text("Questo utente è già in muto!")
     else:
-        message.reply_text("This user isn't in the chat!")
+        message.reply_text("Questo utente non esiste in questa chat!")
 
     return ""
 
@@ -67,20 +67,20 @@ def unmute(bot: Bot, update: Update, args: List[str]) -> str:
 
     user_id = extract_user(message, args)
     if not user_id:
-        message.reply_text("You'll need to either give me a username to unmute, or reply to someone to be unmuted.")
+        message.reply_text("Dovrai o darmi un nome utente per mutarlo o rispondere a qualcuno che deve essere mutato.")
         return ""
 
     member = chat.get_member(int(user_id))
 
     if member:
         if is_user_admin(chat, user_id, member=member):
-            message.reply_text("This is an admin, what do you expect me to do?")
+            message.reply_text("Questo è un admin, cosa ti aspetti che faccia?")
             return ""
 
         elif member.status != 'kicked' and member.status != 'left':
             if member.can_send_messages and member.can_send_media_messages \
                     and member.can_send_other_messages and member.can_add_web_page_previews:
-                message.reply_text("This user already has the right to speak.")
+                message.reply_text("Questo utente può parlare.")
                 return ""
             else:
                 bot.restrict_chat_member(chat.id, int(user_id),
@@ -88,7 +88,7 @@ def unmute(bot: Bot, update: Update, args: List[str]) -> str:
                                          can_send_media_messages=True,
                                          can_send_other_messages=True,
                                          can_add_web_page_previews=True)
-                message.reply_text("Unmuted!")
+                message.reply_text("Muto disattivato!")
                 return "<b>{}:</b>" \
                        "\n#UNMUTE" \
                        "\n<b>Admin:</b> {}" \
@@ -96,8 +96,7 @@ def unmute(bot: Bot, update: Update, args: List[str]) -> str:
                                                   mention_html(user.id, user.first_name),
                                                   mention_html(member.user.id, member.user.first_name))
     else:
-        message.reply_text("This user isn't even in the chat, unmuting them won't make them talk more than they "
-                           "already do!")
+        message.reply_text("Questo utente non è nella chat, unmutarlo non gli permetterà di parlare!!")
 
     return ""
 
@@ -115,28 +114,28 @@ def temp_mute(bot: Bot, update: Update, args: List[str]) -> str:
     user_id, reason = extract_user_and_text(message, args)
 
     if not user_id:
-        message.reply_text("You don't seem to be referring to a user.")
+        message.reply_text("Non ti stai riferendo ad un utente")
         return ""
 
     try:
         member = chat.get_member(user_id)
     except BadRequest as excp:
         if excp.message == "User not found":
-            message.reply_text("I can't seem to find this user")
+            message.reply_text("Non riesco a trovare questo utente")
             return ""
         else:
             raise
 
     if is_user_admin(chat, user_id, member):
-        message.reply_text("I really wish I could mute admins...")
+        message.reply_text("Mi piacerebbe tanto poter mutare gli admin a volte...")
         return ""
 
     if user_id == bot.id:
-        message.reply_text("I'm not gonna MUTE myself, are you crazy?")
+        message.reply_text("Non mi muterò da solo.. Sei pazzo?")
         return ""
 
     if not reason:
-        message.reply_text("You haven't specified a time to mute this user for!")
+        message.reply_text("NOn hai specificato per quanto tempo mutare questo utente!")
         return ""
 
     split_reason = reason.split(None, 1)
@@ -159,35 +158,35 @@ def temp_mute(bot: Bot, update: Update, args: List[str]) -> str:
           "\n<b>Time:</b> {}".format(html.escape(chat.title), mention_html(user.id, user.first_name),
                                      mention_html(member.user.id, member.user.first_name), time_val)
     if reason:
-        log += "\n<b>Reason:</b> {}".format(reason)
+        log += "\n<b>Motivo:</b> {}".format(reason)
 
     try:
         if member.can_send_messages is None or member.can_send_messages:
             bot.restrict_chat_member(chat.id, user_id, until_date=mutetime, can_send_messages=False)
-            message.reply_text("Muted for {}!".format(time_val))
+            message.reply_text("Mutato per {}!".format(time_val))
             return log
         else:
-            message.reply_text("This user is already muted.")
+            message.reply_text("Questo utente è già mutato.")
 
     except BadRequest as excp:
         if excp.message == "Reply message not found":
             # Do not reply
-            message.reply_text("Muted for {}!".format(time_val), quote=False)
+            message.reply_text("Mutato per {}!".format(time_val), quote=False)
             return log
         else:
             LOGGER.warning(update)
             LOGGER.exception("ERROR muting user %s in chat %s (%s) due to %s", user_id, chat.title, chat.id,
                              excp.message)
-            message.reply_text("Well damn, I can't mute that user.")
+            message.reply_text("Diamine, non posso mutare questo utente.")
 
     return ""
 
 
 __help__ = """
 *Admin only:*
- - /mute <userhandle>: silences a user. Can also be used as a reply, muting the replied to user.
- - /tmute <userhandle> x(m/h/d): mutes a user for x time. (via handle, or reply). m = minutes, h = hours, d = days.
- - /unmute <userhandle>: unmutes a user. Can also be used as a reply, muting the replied to user.
+ - /mute <userhandle>: silenzia un utente. Può anche essere usato come risposta, disattivando la risposta all'utente.
+ - /tmute <userhandle> x (m / h / d): silenzia un utente per x tempo. (tramite handle o risposta). m = minuti, h = ore, d = giorni.
+ - /unmute <userhandle>: riattiva un utente. Può anche essere usato come risposta, disattivando la risposta all'utente.
 """
 
 __mod_name__ = "Muting"
