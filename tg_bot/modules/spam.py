ec49d4f742bcfb40804b4fc6ec36b160d4a4f3d2
Christian Cavuti <christian.cavuti@gmail.com>
2019-07-18 23:01:22 +0200

    Added filters to url MessageEntity for locks module

--- tg_bot/modules/spam.py
+++ tg_bot/modules/spam.py
@@ -1,17 +1,40 @@
 from tg_bot import dispatcher, LOGGER
+from tg_bot.modules.helper_funcs.chat_status import bot_can_delete
 from telegram import Update, Bot
-from telegram.ext import MessageHandler, Filters
+from telegram.ext import MessageHandler, Filters, CommandHandler
+from telegram.ext.dispatcher import run_async
 
 
+FORBIDDEN_ENTITY_TYPES = ['url', 'text_link', 'email', 'phone_number']
+
+@run_async
+@bot_can_delete
 def spam_filter(bot: Bot, update: Update):
     msg = update.effective_message.text
-    LOGGER.debug("spam" in msg)
-    if msg and "spam" in msg:
-        user = update.effective_user.username
-        update.effective_message.reply_text(f"Non spammare! @{user}", quote=False)
-        update.effective_message.delete()
+    message_entities = update.effective_message.parse_entities()
+    message_caption_entities = update.effective_message.parse_caption_entities()
 
+    found = False
+    for descriptor, entity in message_entities.items():
+        LOGGER.debug(f"Found message entity: {descriptor['type']} {entity}")
+        found = True
+
+    if found:
+        spam_action(update)
+
+
+def spam_action(update: Update):
+    user = update.effective_user.username
+    update.effective_message.reply_text(f"Non spammare! @{user}", quote=False)
+    update.effective_message.delete()
+
+
+def white_spam_add(bot: Bot, update: Update):
+    if update.effective_message.reply_to_message:
+        pass
+    else:
+        update.effective_message.reply_text("Cosa vuoi aggiungere in whitelist?")
 
 SPAM_HANDLER = MessageHandler(Filters.all & Filters.group, spam_filter)
-
-dispatcher.add_handler(SPAM_HANDLER)
+WHITE_SPAM_HANDLER = CommandHandler("whitespam", white_spam_add)
+#dispatcher.add_handler(SPAM_HANDLER)
