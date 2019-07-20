884c8fcd5c5ed58c1e0dc6b6fd985c4fe73b10c2
Mattia <mattiafailla@hotmail.it>
2019-07-20 16:05:15 +0200

    Traduzione sub-modulo extraction

--- tg_bot/modules/helper_funcs/extraction.py
+++ tg_bot/modules/helper_funcs/extraction.py
@@ -49,8 +49,8 @@ def extract_user_and_text(message: Message, args: List[str]) -> (Optional[int], 
         user = args[0]
         user_id = get_user_id(user)
         if not user_id:
-            message.reply_text("I don't have that user in my db. You'll be able to interact with them if "
-                               "you reply to that person's message instead, or forward one of that user's messages.")
+            message.reply_text("Non ho quell'utente nel mio db. Sarai in grado di interagire con lui se "
+                               "rispondi al messaggio di quella persona o mi inoltri uno dei messaggi di quell'utente.")
             return None, None
 
         else:
@@ -75,9 +75,9 @@ def extract_user_and_text(message: Message, args: List[str]) -> (Optional[int], 
         message.bot.get_chat(user_id)
     except BadRequest as excp:
         if excp.message in ("User_id_invalid", "Chat not found"):
-            message.reply_text("I don't seem to have interacted with this user before - please forward a message from "
-                               "them to give me control! (like a voodoo doll, I need a piece of them to be able "
-                               "to execute certain commands...)")
+            message.reply_text("Non ho mai interagito con questo utente prima - per favore inoltrami un suo messaggio "
+                               "per permettermi di prendere il controllo! (come una bambola voodoo, ho bisogno di un piccolo pezzo di questo utente "
+                               "per eseguire alcuni comandi..)")
         else:
             LOGGER.exception("Exception %s on user %s", excp.message, user_id)
 
