2ff2ab61021e03411531f381981c8044be7dc92c
Mattia <mattiafailla@hotmail.it>
2019-07-27 01:31:22 +0200

    Updating the banhammer sticker

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -36,7 +36,7 @@ class Config(object):
     DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
     STRICT_GBAN = False
     WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
-    BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
+    BAN_STICKER = 'CAADAgADXQIAAtzyqweiDpkj4KP25wI'  # banhammer electus sticker
     ALLOW_EXCL = False  # Allow ! commands as well as /
 
 
