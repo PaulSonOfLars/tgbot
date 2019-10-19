e383c2198c1650ba00fdd0893c62dff1d86bf1b2
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-10-19 20:14:03 +0530

    fix for the caption of the media to get saved

--- tg_bot/modules/helper_funcs/msg_types.py
+++ tg_bot/modules/helper_funcs/msg_types.py
@@ -99,32 +99,32 @@ def get_welcome_type(msg: Message):
 
     elif msg.reply_to_message and msg.reply_to_message.sticker:
         content = msg.reply_to_message.sticker.file_id
-        text = msg.reply_to_message.text
+        text = msg.reply_to_message.caption
         data_type = Types.STICKER
 
     elif msg.reply_to_message and msg.reply_to_message.document:
         content = msg.reply_to_message.document.file_id
-        text = msg.reply_to_message.text
+        text = msg.reply_to_message.caption
         data_type = Types.DOCUMENT
 
     elif msg.reply_to_message and msg.reply_to_message.photo:
         content = msg.reply_to_message.photo[-1].file_id  # last elem = best quality
-        text = msg.reply_to_message.text
+        text = msg.reply_to_message.caption
         data_type = Types.PHOTO
 
     elif msg.reply_to_message and msg.reply_to_message.audio:
         content = msg.reply_to_message.audio.file_id
-        text = msg.reply_to_message.text
+        text = msg.reply_to_message.caption
         data_type = Types.AUDIO
 
     elif msg.reply_to_message and msg.reply_to_message.voice:
         content = msg.reply_to_message.voice.file_id
-        text = msg.reply_to_message.text
+        text = msg.reply_to_message.caption
         data_type = Types.VOICE
 
     elif msg.reply_to_message and msg.reply_to_message.video:
         content = msg.reply_to_message.video.file_id
-        text = msg.reply_to_message.text
+        text = msg.reply_to_message.caption
         data_type = Types.VIDEO
 
     return text, data_type, content, buttons
