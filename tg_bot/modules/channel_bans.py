8a94daae02f1c6546d7584a9906e479637e8d3a5
barfu296 <fubar213@protonmail.com>
2022-07-04 17:44:55 +0400

    fix: global ban for sudousers

--- tg_bot/modules/channel_bans.py
+++ tg_bot/modules/channel_bans.py
@@ -4,7 +4,7 @@ from telegram import Chat, Update, Bot
 from telegram.ext import CommandHandler, MessageHandler, Filters
 from telegram.ext.dispatcher import run_async
 
-from tg_bot import dispatcher
+from tg_bot import dispatcher, SUDO_USERS
 from tg_bot.modules.helper_funcs.chat_status import user_not_admin, user_admin, bot_can_delete
 from tg_bot.modules.log_channel import loggable
 from tg_bot.modules.sql import channel_bans_sql as sql
@@ -20,15 +20,6 @@ def ban_channel(bot: Bot, update: Update, args: List[str]):
     update.effective_message.reply_text("Channel " + channel_name + "was added to ban list")
 
 
-@user_admin
-@bot_can_delete
-@loggable
-def global_ban_channel(bot: Bot, update: Update, args: List[str]):
-    channel_name = args[0]
-    sql.add_channel_global_bans(channel_name)
-    update.effective_message.reply_text("Channel " + channel_name + "was added to global ban list")
-
-
 @run_async
 @user_admin
 @loggable
@@ -37,28 +28,6 @@ def unban_channel(bot: Bot, update: Update, args: List[str]):
     chat_id = update.effective_chat.id
     sql.delete_channel_ban(chat_id, channel_name)
     update.effective_message.reply_text("Channel " + channel_name + "was removed from ban list")
-
-
-@run_async
-@user_admin
-@loggable
-def global_unban_channel(bot: Bot, update: Update, args: List[str]):
-    channel_name = args[0]
-    sql.delete_channel_global_ban(channel_name)
-    update.effective_message.reply_text("Channel " + channel_name + "was removed from global ban list")
-
-
-@run_async
-def remove_banned_forwardings(bot: Bot, update: Update):
-    forwarder_from_channel_name = update.effective_message.forward_from_chat.username
-    is_exists = sql.is_channel_ban_exists(update.effective_chat.id, forwarder_from_channel_name)
-    if is_exists:
-        update.effective_message.reply_text("Channel " + forwarder_from_channel_name + " banned in this channel")
-        update.effective_message.delete()
-    is_exists_in_global = sql.is_global_channel_ban_exists(forwarder_from_channel_name)
-    if is_exists_in_global:
-        update.effective_message.reply_text("Channel " + forwarder_from_channel_name + " banned in global ban list")
-        update.effective_message.delete()
 
 
 @run_async
@@ -73,15 +42,56 @@ def banned_channels(bot: Bot, update: Update):
         update.effective_message.reply_text("There are no channels in ban")
 
 
+@user_admin
+@bot_can_delete
+@loggable
+def global_ban_channel(bot: Bot, update: Update, args: List[str]):
+    if int(update.effective_user.id) in SUDO_USERS:
+        update.effective_message.reply_text(
+            "You are not a sudo user")
+    channel_name = args[0]
+    sql.add_channel_global_bans(channel_name)
+    update.effective_message.reply_text("Channel " + channel_name + "was added to global ban list")
+
+
+@run_async
+@user_admin
+@loggable
+def global_unban_channel(bot: Bot, update: Update, args: List[str]):
+    if int(update.effective_user.id) in SUDO_USERS:
+        update.effective_message.reply_text(
+            "You are not a sudo user")
+    channel_name = args[0]
+    sql.delete_channel_global_ban(channel_name)
+    update.effective_message.reply_text("Channel " + channel_name + "was removed from global ban list")
+
+
 @run_async
 @user_admin
 def global_banned_channels(bot: Bot, update: Update):
+    if int(update.effective_user.id) in SUDO_USERS:
+        update.effective_message.reply_text(
+            "You are not a sudo user")
     res = sql.get_channel_global_bans()
     if res is not None:
         res = list(map(lambda x: x.channel_to_ban, res))
         update.effective_message.reply_text("Channels banned globally: " + ','.join(res))
     else:
         update.effective_message.reply_text("There are no channels in ban")
+
+
+@run_async
+def remove_banned_forwardings(bot: Bot, update: Update):
+    if update.effective_message.forward_from_chat is not None:
+        forwarder_from_channel_name = update.effective_message.forward_from_chat.username
+        is_exists = sql.is_channel_ban_exists(update.effective_chat.id, forwarder_from_channel_name)
+        if is_exists:
+            update.effective_message.reply_text("Channel " + forwarder_from_channel_name + " banned in this channel")
+            update.effective_message.delete()
+        is_exists_in_global = sql.is_global_channel_ban_exists(forwarder_from_channel_name)
+        if is_exists_in_global:
+            update.effective_message.reply_text("Channel " + forwarder_from_channel_name + " banned in global ban list")
+            update.effective_message.delete()
 
 
 def __migrate__(old_chat_id, new_chat_id):
@@ -105,7 +115,8 @@ BAN_CHANNEL_HANDLER = CommandHandler("banchannel", ban_channel, pass_args=True, 
 UNBAN_CHANNEL_HANDLER = CommandHandler("unbanchannel", unban_channel, pass_args=True, filters=Filters.group)
 BANNED_CHANNELS_HANDLER = CommandHandler("bannedchannels", banned_channels, filters=Filters.group)
 GLOBAL_BAN_CHANNEL_HANDLER = CommandHandler("globalbanchannel", ban_channel, pass_args=True, filters=Filters.group)
-GLOBAL_UNBAN_CHANNEL_HANDLER = CommandHandler("globalunbanchannel", unban_channel, pass_args=True, filters=Filters.group)
+GLOBAL_UNBAN_CHANNEL_HANDLER = CommandHandler("globalunbanchannel", unban_channel, pass_args=True,
+                                              filters=Filters.group)
 GLOBAL_BANNED_CHANNELS_HANDLER = CommandHandler("globalbannedchannels", banned_channels, filters=Filters.group)
 
 dispatcher.add_handler(BAN_CHANNEL_HANDLER)
