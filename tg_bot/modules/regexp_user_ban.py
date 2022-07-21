17ee15eadacaf1ebd545fac9389bf7adf64a7fac
bezpalovski <109282701+bezpalovski@users.noreply.github.com>
2022-07-21 21:16:50 +0300

    Update regexp_user_ban.py
    Needs SUDO_USERS to work

--- tg_bot/modules/regexp_user_ban.py
+++ tg_bot/modules/regexp_user_ban.py
@@ -7,7 +7,7 @@ from telegram.error import BadRequest
 from telegram.ext import run_async, CommandHandler, Filters, MessageHandler
 from telegram.utils.helpers import mention_html
 
-from tg_bot import dispatcher, BAN_STICKER, LOGGER
+from tg_bot import dispatcher, BAN_STICKER, LOGGER, SUDO_USERS
 from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot.modules.helper_funcs.chat_status import bot_admin, user_admin, is_user_ban_protected, can_restrict, \
     is_user_admin, is_user_in_chat, bot_can_delete
