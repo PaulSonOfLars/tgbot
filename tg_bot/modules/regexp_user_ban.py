96b47375a40dc94367337544c8903ac217213fa0
wrwrabbit <76569803+wrwrabbit@users.noreply.github.com>
2022-07-21 14:37:06 +0300

    Update regexp_user_ban.py

--- tg_bot/modules/regexp_user_ban.py
+++ tg_bot/modules/regexp_user_ban.py
@@ -123,7 +123,7 @@ def remove_banned_nicknames(bot: Bot, update: Update):
                 is_banned = re.match(regex, name.username)
 
                 if is_banned:
-                    update.effective_message.reply_text("You're not welcome here!")
+                    update.effective_message.reply_text("#бан_банан тебе 🍌")
                     chat.kick_member(name.id)
                     break
 
