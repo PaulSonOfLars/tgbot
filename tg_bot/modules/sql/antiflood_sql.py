3366b538c3434584ddd5a672eb2d074f9005202a
barfu296 <fubar213@protonmail.com>
2022-08-30 22:48:29 +0300

    fix: antiflood

--- tg_bot/modules/sql/antiflood_sql.py
+++ tg_bot/modules/sql/antiflood_sql.py
@@ -6,13 +6,11 @@ from tg_bot.modules.sql import BASE, SESSION
 
 DEF_COUNT = 0
 DEF_LIMIT = 0
-DEF_OBJ = (None, DEF_COUNT, DEF_LIMIT)
 
 
 class FloodControl(BASE):
     __tablename__ = "antiflood"
     chat_id = Column(String(14), primary_key=True)
-    user_id = Column(BigInteger)
     count = Column(BigInteger, default=DEF_COUNT)
     limit = Column(BigInteger, default=DEF_LIMIT)
 
@@ -35,49 +33,40 @@ def set_flood(chat_id, amount):
         flood = SESSION.query(FloodControl).get(str(chat_id))
         if not flood:
             flood = FloodControl(str(chat_id))
-
-        flood.user_id = None
         flood.limit = amount
-
-        CHAT_FLOOD[str(chat_id)] = (None, DEF_COUNT, amount)
-
+        CHAT_FLOOD[str(chat_id)] = (None, None, DEF_COUNT, amount)
         SESSION.add(flood)
         SESSION.commit()
 
 
-def update_flood(chat_id: str, user_id) -> bool:
+def update_flood(chat_id: str, user_id, is_channel) -> bool:
     if str(chat_id) in CHAT_FLOOD:
-        curr_user_id, count, limit = CHAT_FLOOD.get(str(chat_id), DEF_OBJ)
-
-        if limit == 0:  # no antiflood
+        curr_user_id, curr_is_channel, count, limit = CHAT_FLOOD[str(chat_id)]
+        if limit == 0:
             return False
-
-        if user_id != curr_user_id or user_id is None:  # other user
-            CHAT_FLOOD[str(chat_id)] = (user_id, DEF_COUNT + 1, limit)
+        if (user_id != curr_user_id and is_channel != curr_is_channel) or user_id is None:  # other user
+            CHAT_FLOOD[str(chat_id)] = (user_id, is_channel, DEF_COUNT + 1, limit)
             return False
-
         count += 1
         if count > limit:  # too many msgs, kick
-            CHAT_FLOOD[str(chat_id)] = (None, DEF_COUNT, limit)
+            CHAT_FLOOD[str(chat_id)] = (None, None, DEF_COUNT, limit)
             return True
-
         # default -> update
-        CHAT_FLOOD[str(chat_id)] = (user_id, count, limit)
+        CHAT_FLOOD[str(chat_id)] = (user_id, is_channel, count, limit)
         return False
 
 
 def get_flood_limit(chat_id):
-    return CHAT_FLOOD.get(str(chat_id), DEF_OBJ)[2]
+    return CHAT_FLOOD[str(chat_id)][2]
 
 
 def migrate_chat(old_chat_id, new_chat_id):
     with INSERTION_LOCK:
         flood = SESSION.query(FloodControl).get(str(old_chat_id))
         if flood:
-            CHAT_FLOOD[str(new_chat_id)] = CHAT_FLOOD.get(str(old_chat_id), DEF_OBJ)
+            CHAT_FLOOD[str(new_chat_id)] = CHAT_FLOOD[str(old_chat_id)]
             flood.chat_id = str(new_chat_id)
             SESSION.commit()
-
         SESSION.close()
 
 
@@ -85,7 +74,7 @@ def __load_flood_settings():
     global CHAT_FLOOD
     try:
         all_chats = SESSION.query(FloodControl).all()
-        CHAT_FLOOD = {chat.chat_id: (None, DEF_COUNT, chat.limit) for chat in all_chats}
+        CHAT_FLOOD = {chat.chat_id: (None, None, DEF_COUNT, chat.limit) for chat in all_chats}
     finally:
         SESSION.close()
 
